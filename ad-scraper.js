a5d55a9fda3badf3de17a369d31263f6eb330cf2
Jesse Meade-Clift <jesse@zone4.ca>
2018-03-12 19:13:29 -0600

    Fix incorrect reference

--- ad-scraper.js
+++ ad-scraper.js
@@ -49,7 +49,7 @@ function parseHTML(html) {
         });
         adData.VIP.adAttributes.forEach(function(a) {
             var attr = a.localeSpecificValues.en;
-            ad.info[attr.label] = attr.value || attr.machineValue;
+            ad.info[attr.label] = attr.value || a.machineValue;
         });
 
         // Other attributes of interest (I'm attempting to match the old output)
